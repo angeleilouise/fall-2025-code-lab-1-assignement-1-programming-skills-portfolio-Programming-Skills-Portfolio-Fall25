92759b5d9b9b8e036ecf155cbff7f3e190dacda2
angeleilouise <angelei.louisecdlimpin@gmail.com>
2025-11-22 16:25:37 +0400

    Python Exercises
    Introduction to Programming Assessment 1: In this assessment, I completed 10 coding exercises that made use of the basic python programming language skills and techniques. Thus, this assessment served as a tool for me to practice and enhance my programming skills.

--- Exercise 09 -Hello/Exercise 9.py
+++
@@ -1,9 +0,0 @@
-def hello():
-    print("Hello") ##print hello using a print statement 
-
-def main():
-    hello()  ##display and call for the hello() function
-
-if __name__ == "__main__":
-    main() 
-
