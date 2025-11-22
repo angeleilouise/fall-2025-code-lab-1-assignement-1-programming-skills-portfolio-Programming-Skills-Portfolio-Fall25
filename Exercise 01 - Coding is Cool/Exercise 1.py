fb53d543f9abf4f8fc85d0c148819625b681eaf7
angeleilouise <angelei.louisecdlimpin@gmail.com>
2025-11-22 16:21:39 +0400

    Delete outdated file

--- Exercise 01 - Coding is Cool/Exercise 1.py
+++
@@ -1,7 +0,0 @@
-#assign the given words in variables named: word#
-word1 = "Coding"
-word2 = "is"
-word3 = "Cool"
-print (word1 + word2 + word3)
-
-print (F"{word1} {word2} {word3}") #fstring is used 
